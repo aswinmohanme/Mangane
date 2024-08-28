6a0ed96b765b52ed0265fe2fd1514c90a4c660f2
aswinmohanme <dev.aswinmohan@gmail.com>
2024-08-28 19:41:46 +0000

    Deploying to gh-pages from @ aswinmohanme/Mangane@9215fac1490c89f6a9e3f0c3675466e0e54617df 🚀

---
+++ packs/js/locale_whitelist_sk-json-946c24af28e03d5b12c8.chunk.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkmangane_fe"] = self["webpackChunkmangane_fe"] || []).push([[190],{
+
+/***/ 1588:
+/*!***********************************************!*\
+  !*** ./app/soapbox/locales/whitelist_sk.json ***!
+  \***********************************************/
+/***/ (function(module) {
+
+module.exports = [];
+
+/***/ })
+
+}]);
